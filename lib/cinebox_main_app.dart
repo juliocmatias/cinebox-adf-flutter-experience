01996dc3c7e0f58553bd6792b859823e9b6ef21e
Julio Matias <juliocesark2@hotmail.com>
2025-08-13 23:06:33 -0300

    feat: criar a classe CineboxMainApp como ponto de entrada da aplicação

---
+++ lib/cinebox_main_app.dart
@@ -0,0 +1,18 @@
+import 'package:cinebox/ui/core/themes/theme.dart';
+import 'package:cinebox/ui/splash/splash_scream.dart';
+import 'package:flutter/material.dart';
+
+class CineboxMainApp extends StatelessWidget {
+  const CineboxMainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CineBox',
+      theme: AppTheme.theme,
+      routes: {
+        '/': (_) => SplashScream(),
+      },
+    );
+  }
+}
